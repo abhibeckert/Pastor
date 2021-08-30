41f82966b3002be56712503660945dff4eb5901f
Abhi Beckert <abhi@inspectionapps.com>
2021-08-31 09:47:12 +1000

    Initial Commit

---
+++ Pastor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pastor
+//
+//  Created by Abhi Beckert on 21-8-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
